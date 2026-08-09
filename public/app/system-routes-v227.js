35067e495aa2c7676948dac7984d57074684048d
cerbanimo-dev <cerbanimo@gmail.com>
2026-08-08 20:09:28 -0400

    Synchronize system routes with v1.0.57

--- public/app/system-routes-v227.js
+++ public/app/system-routes-v227.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='1.0.56';
+const VERSION='1.0.57';
 const REVISION='five-system-route-contract-v227';
 const BOOT_KEY='civweave.install-boundary.boot.v227';
 const ROUTES=Object.freeze({
