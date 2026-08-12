ee9df8dd212b431be6988b03b0225588ffd96091
cerbanimo-dev <cerbanimo@gmail.com>
2026-08-12 23:41:17 +0000

    Materialize Civweave 1.0.122 return-safe release

--- public/app/install-boundary-v146.js
+++ public/app/install-boundary-v146.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION='1.0.121';
+const VERSION='1.0.122';
 const REVISION='chat-convergence-v250-navigation-lifecycle-v424';
 const params=new URLSearchParams(location.search);
 const requestedRelease=/^\d+\.\d+\.\d+$/.test(params.get('version')||'')?params.get('version'):VERSION;
