ea74df5a7cf7565e6ef3433ccccb4f35e4f5eec7
cerbanimo-dev <cerbanimo@gmail.com>
2026-08-12 23:19:09 +0000

    Prepare Civweave 1.0.121 desktop and scrollbar release

--- public/service-worker-core-v208.js
+++ public/service-worker-core-v208.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '1.0.119';
+const VERSION = '1.0.121';
 const BUILD = 'lightweight-shell-v208-installer-brand-v1';
 const SHELL_CACHE = `civweave-shell-${VERSION}-${BUILD}`;
 const RUNTIME_CACHE = `civweave-runtime-${VERSION}-${BUILD}`;
