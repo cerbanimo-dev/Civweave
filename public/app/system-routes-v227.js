c818898f6821baeec135a95629c4eb370f157b8c
cerbanimo-dev <cerbanimo@gmail.com>
2026-08-16 07:29:32 -0400

    Recognize Civweave Guild Quest surface

--- public/app/system-routes-v227.js
+++ public/app/system-routes-v227.js
@@ -11,6 +11,7 @@ const ROUTES=Object.freeze({
   anarchadia:Object.freeze({id:'anarchadia',label:'Anarchadia',pathname:'/app/anarchadia-console-v139.html',params:Object.freeze({cabinet:'1'})})
 });
 const PATH_TO_ID=new Map(Object.values(ROUTES).map(route=>[route.pathname,route.id]));
+PATH_TO_ID.set('/app/civweave-guild-quest-v1.html','civweave');
 function normalizePathname(value){
   let pathname=String(value||'/').split(/[?#]/,1)[0]||'/';
   try{pathname=decodeURI(pathname)}catch{}
